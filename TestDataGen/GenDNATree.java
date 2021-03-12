528e00578f49fc9b6499d0d1546a631069dbf3c6
Nagendra <>
2021-03-12 00:29:33 -0500

    Final working in local

--- TestDataGen/GenDNATree.java
+++ TestDataGen/GenDNATree.java
@@ -1,25 +1,27 @@
 import java.util.Random;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.nio.file.Files;
 import java.io.File;
 
 public class GenDNATree {
     public static void main(String... args) throws Exception {
+        for(int i=1;i<=4;i++){
         char[] chars = new char[] { 'A', 'C', 'G', 'T' };
-        HashSet<String> lines = new HashSet<>();
+        ArrayList<String> lines = new ArrayList<>();
         Random cRandom = new Random();
         Random lRandom = new Random();
-        for (int line = 1; line <= 12948; line++) {
+        for (int line = 1; line <= 7000; line++) {
             int length = lRandom.nextInt(17) + 1;
             String text = "";
             for (int l = 1; l <= length; l++) {
                 text += chars[cRandom.nextInt(chars.length)];
-            }
-            
+            }       
             lines.add("insert " + text);
         }
+        lines.add("print");
         Files.write(new File(
-            "input.txt")
+            "input_"+i+".txt")
                 .toPath(), lines);
+    }
     }
 }
